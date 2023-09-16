3ef57edd1be1bd51bca8704d4fefb0d23fd70fb6
Pedro Gros <pedro_gros1998@hotmail.com>
2023-09-16 19:37:13 -0300

    Add architecture Reg16bits

--- Eq06-RegFile/Eq06-reg16bits.vhd
+++ Eq06-RegFile/Eq06-reg16bits.vhd
@@ -18,11 +18,25 @@ entity Register is
 	   out    	    : out unsigned(15 downto 0); -- Saída de Dados
 	   clk   	    : in  std_logic;   			 -- Condicao
 	   rst			: in  std_logic;			 -- Flag
-	   wr_en	    : in  std_logic				 -- 
+	   wr_en	    : in  std_logic				 -- Write enable
 	  );
 end entity;
 
 
 architecture a_Register of Register is
+	signal register: unsigned(15 downto 0);
+begin
+
+	process(clk, rst, wr_en)
 	begin
+		if rst = '1' then
+		register <= "0000000000000000";
+		elsif wr_en = '1' then
+			if rising_edge(clk) then
+				register <= in;
+			end if;
+		end if;
+	end process;
+
+	out <= register;
 end architecture;
