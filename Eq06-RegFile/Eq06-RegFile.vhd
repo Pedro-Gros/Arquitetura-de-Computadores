f2dbad2c8a056b9960139ae9d03b64e9db0fbc2e
Pedro Henrique Grossi da Silva <ph02grossi@gmail.com>
2023-09-17 15:24:48 -0300

    Finalização dos componentes - Precisei mudar o nome da entity Register e do signal register para register_comp e s_register respectivamente para conseguir compilar. Aparentemente a palavra register é reservada e estava dando problema. - Precisei mudar o nome dos sinais in e out de entrada no registrador para reg_in e reg_out tambem por "in" e "out" serem palavras reservadas. - Mudei o nome do sinal rst do componente register_comp para rst_reg para diferenciar do rst do componente RegistersBank. - Adicionei o process para:     1 - Condicional de rst que zera todos os registradores     2 - Condicional de escrita que seleciona qual registrador vai ser         escrito. - Apos reler na atividade o registrador 0(zero) não pode ser modificado para nada diferente de 0, por esse motivo não deixei modificar esse registrador para um valor diferente de 0(zero); - Acho que precisariamos gerantir que quando o componente iniciar o registrador 0(zero) já seja iniciado com o valor 0(zero), mas não estou conseguindo pensar em como fazer isso.

--- Eq06-RegFile/Eq06-RegFile.vhd
+++ Eq06-RegFile/Eq06-RegFile.vhd
@@ -31,14 +31,14 @@ end entity;
 architecture a_RegistersBank of RegistersBank is
 
 	-- Declaração do componente registrador
-	component Register is
-		port( in			: in  unsigned(15 downto 0); -- Entrada de Dados
-			  out   		: out unsigned(15 downto 0); -- Saída de Dados
-			  clk			: in  std_logic;   			 -- Condicao
-			  rst			: in  std_logic;			 -- Flag
-			  wr_en			: in  std_logic				 -- Write enable
-			 );
-	   end component Register;
+	component register_comp is
+		port(	reg_in			: in  unsigned(15 downto 0); -- Entrada de Dados
+				reg_out    	    : out unsigned(15 downto 0); -- Saída de Dados
+				clk   	    	: in  std_logic;   			 -- Condicao
+				rst_reg			: in  std_logic;			 -- Flag
+				wr_en	    	: in  std_logic				 -- Write enable
+				);
+	end component register_comp;
 
 	-- Sinais de entrada dos registradores
 	signal in_0, in_1, in_2, in_3, in_4, in_5, in_6, in_7			: unsigned(15 downto 0);
@@ -47,21 +47,21 @@ architecture a_RegistersBank of RegistersBank is
 
 	begin
 		-- Instanciando registrador 0
-		rg0: Register port map(in_0, out_0, CLK, rst, WE3);
+		rg0: register_comp port map(in_0, out_0, CLK, rst, WE3);
 		-- Instanciando registrador 1
-		rg1: Register port map(in_1, out_1, CLK, rst, WE3);
+		rg1: register_comp port map(in_1, out_1, CLK, rst, WE3);
 		-- Instanciando registrador 2
-		rg2: Register port map(in_2, out_2, CLK, rst, WE3);
+		rg2: register_comp port map(in_2, out_2, CLK, rst, WE3);
 		-- Instanciando registrador 3
-		rg3: Register port map(in_3, out_3, CLK, rst, WE3);
+		rg3: register_comp port map(in_3, out_3, CLK, rst, WE3);
 		-- Instanciando registrador 4
-		rg4: Register port map(in_4, out_4, CLK, rst, WE3);
+		rg4: register_comp port map(in_4, out_4, CLK, rst, WE3);
 		-- Instanciando registrador 5
-		rg5: Register port map(in_5, out_5, CLK, rst, WE3);
+		rg5: register_comp port map(in_5, out_5, CLK, rst, WE3);
 		-- Instanciando registrador 6
-		rg6: Register port map(in_6, out_6, CLK, rst, WE3);
+		rg6: register_comp port map(in_6, out_6, CLK, rst, WE3);
 		-- Instanciando registrador 7
-		rg7: Register port map(in_7, out_7, CLK, rst, WE3);
+		rg7: register_comp port map(in_7, out_7, CLK, rst, WE3);
 
 		-- Saida 1 recebe o valor do registrador selecionado por A1
 		RD1 <= 	out_0 when A1 = "000" else
@@ -85,18 +85,31 @@ architecture a_RegistersBank of RegistersBank is
 				out_7 when A2 = "111" else
 				"0000000000000000";
 
-		-- ISSO SO ACONTECE CASO O WE3 FOR IGUAL A 1, PRECISA DE UM PROCESS AQUI PARA FAZER O IF
-		-- PRECISA TAMBEM FAZER A CONDIÇÃO DE RESET QUE ZERA TODOS OS REGISTRADORES
-		
-		-- Entrada WD3 é enviada para a entrada do registrador selecionado por A3
-		WD3 => 	in_0 when A3 = "000" else -- registrador 0 é sempre 0? Acho que esta escrito na atividade mas agora estou compreguiça de verificar. Ver depois!!!!
-				in_1 when A3 = "001" else
-				in_2 when A3 = "010" else
-				in_3 when A3 = "011" else
-				in_4 when A3 = "100" else
-				in_5 when A3 = "101" else
-				in_6 when A3 = "110" else
-				in_7 when A3 = "111" else
-				"0000000000000000";
-
+		process(rst, WE3, A3)
+		begin
+			-- Caso a flag de reset esteja habilitada, reseta todos os registradores para 0(zero)!
+			if rst = '1' then
+				in_0 <= "0000000000000000";
+				in_1 <= "0000000000000000";
+				in_2 <= "0000000000000000";
+				in_3 <= "0000000000000000";
+				in_4 <= "0000000000000000";
+				in_5 <= "0000000000000000";
+				in_6 <= "0000000000000000";
+				in_7 <= "0000000000000000";
+			-- Os registradores são escritos caso a flag de rst não esteja habilitada, mas a flag de escrita esteja!
+			elsif WE3 = '1' then
+				-- Entrada WD3 é enviada para a entrada do registrador selecionado por A3
+				if 		A3 = "001" then in_1 <= WD3;
+				elsif 	A3 = "010" then in_2 <= WD3;
+				elsif 	A3 = "011" then in_3 <= WD3;
+				elsif 	A3 = "100" then in_4 <= WD3;
+				elsif 	A3 = "101" then in_5 <= WD3;
+				elsif 	A3 = "110" then in_6 <= WD3;
+				elsif 	A3 = "111" then in_7 <= WD3;
+				-- registrador 0 sempre possui o valor 0 (zero). Por esse motivo não é possivel modifica-lo!
+				else					in_0 <= "0000000000000000";
+				end if;
+			end if;
+		end process;
 end architecture;
