05b29c4909ad728e89a1e2e740cd84c33f56c5be
PedroGrossi <ph02grossi@gmail.com>
2023-10-08 10:48:04 -0300

    Correcoes para o TestBanch |_ Correcoes em algumas ligacoes do componente     ULARegs |_ Correcoes na implementacao do TestBanch |_ Simulacao criada + Editada corretamente |_ Criacao do .bat

--- Eq06-ULARegs/Eq06-ULARegs.vhd
+++ Eq06-ULARegs/Eq06-ULARegs.vhd
@@ -23,7 +23,7 @@ entity ULARegs is
 		op   		: in unsigned(2  downto 0);	   	-- Seleciona operacao na ULA
 		flags 		: out unsigned(1 downto 0);		-- Flags da ULA
 		in_ext 		: in unsigned(15 downto 0);  	-- Valor de input externo
-		select_MUX 	: in std_logic;				 	-- Selecao do MUX
+		select_MUX 	: in std_logic				 	-- Selecao do MUX
 	  );
 end entity;
 
@@ -56,39 +56,25 @@ architecture a_ULARegs of ULARegs is
 	
 	-- Sinais da ULA
 	signal s_in_A, s_in_B, s_out_ULA 	: unsigned(15 downto 0);
-	signal s_op 						: unsigned(2 downto 0);
-	signal s_flags 						: unsigned(1 downto 0);
 	
 	-- Sinais do RegistersBank
-	signal s_A1, s_A2, s_A3 			: unsigned(2 downto 0);
 	signal s_WD3, s_RD1, s_RD2 			: unsigned(15 downto 0);
-	signal s_WE3, s_CLK, s_rst 			: std_logic := '0';
 	
 	-- Sinais do MUX
 	signal s_out_MUX					: unsigned(15 downto 0);
 	
 	begin
 		-- Instanciando ULA
-		ula: ULA port map(s_in_A, s_in_B, s_op, s_out_ULA, s_flags);
+		ula0: ULA port map(s_in_A, s_in_B, op, s_out_ULA, flags);
 		-- Instanciando RegistersBank
-		rb: RegistersBank port map(s_A1, s_A2, s_WD3, s_A3, s_WE3, s_CLK, s_rst, s_RD1, s_RD2);
+		rb: RegistersBank port map(A1, A2, s_WD3, A3, WE3, CLK, rst, s_RD1, s_RD2);
 		
-		s_out_ULA => s_WD3;
-		s_RD1 => s_in_A;
+		s_WD3 <= s_out_ULA;
+		s_in_A <= s_RD1;
 		
 		s_out_MUX <=	s_RD2 	when select_MUX = '0' else
 						in_ext 	when select_MUX = '1' else
 						"0000000000000000";
 		
-		s_out_MUX => s_in_B;
-		
-		A1 		=> s_A1;
-		A2 		=> s_A2;
-		A3 		=> s_A3;
-		WE3 	=> s_WE3;
-		CLK 	=> s_CLK;
-		rst 	=> s_rst; 
-		op		=> s_op;
-		flags 	=> s_flags;	
-
+		s_in_B <= s_out_MUX;
 end architecture;
